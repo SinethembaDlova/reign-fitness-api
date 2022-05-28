08d04e1ae3937d43668f2f48df46dfecd2a0a666
Sinethemba <sinethemba@wealthmigrate.com>
2022-05-28 19:41:02 +0200

    feature: created database schema

---
+++ schemas/membership.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const  { v4 }  = require('uuid');
+
+const Schema = mongoose.Schema;
+ 
+
+const membershipSchema = new Schema({
+    id: { type: String, unique: true, sparse: true, default: v4() },
+    first_name: { type: String, required: true },
+    last_name: { type: string, required: true },
+    id_number: { type: String, required: true },
+    is_active: { type: Boolean, required: true, default: true },
+    payment_type: { type: String, required: true },
+    bank_account_number: { type: Number },
+    contract: { type: String, required: true },
+    amount: { type: Number, required: true}
+});
+
+const Membership = mongoose.model("Membership", membershipSchema);
+module.exports = Membership;
