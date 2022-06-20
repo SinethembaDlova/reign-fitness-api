d558fae0c521f7eaa02751e13e01c6f59797ae45
Sinethemba <sinethemba@wealthmigrate.com>
2022-06-20 02:05:23 +0200

    feature: enabling cores

--- src/routes/membership.js
+++ src/routes/membership.js
@@ -1,13 +1,16 @@
 
 const express = require("express");
+var cors = require('cors');
 const router = express.Router();
+
+app.use(cors())
 
 const createMembership = require("../controllers/createMembership")
 const fetchMembership = require("../controllers/fetchMembership");
 const fetchAllMemberships = require("../controllers/fetchMemberships");
 
-router.get("/", fetchAllMemberships);
+router.get("/", cors(), fetchAllMemberships);
 router.post("/", createMembership)
-router.get(":id", fetchMembership);
+router.get(":id", cors(), fetchMembership);
 
 module.exports = router;
