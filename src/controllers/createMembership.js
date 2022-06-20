90bdb3e4cdf1579161684761c139302f105290fb
Sinethemba <sinethemba@wealthmigrate.com>
2022-06-20 13:43:46 +0200

    feature: enabling cores

--- src/controllers/createMembership.js
+++ src/controllers/createMembership.js
@@ -12,6 +12,7 @@ module.exports = async (req, res) => {
         amount 
     } = req.body;
     try {
+      res.setHeader('Access-Control-Allow-Origin', '*');
       const membership = await Membership.create({ 
         first_name, 
         last_name, 
@@ -21,7 +22,6 @@ module.exports = async (req, res) => {
         bank_account_number, 
         contract_type,
         amount });
-      res.setHeader('Access-Control-Allow-Origin', '*');
       res.status(201).json({ data: membership, message: "Membership successfully created" });
     } catch (error) {
         res.status(400).json({ error, message: "Failed to create membership." });
