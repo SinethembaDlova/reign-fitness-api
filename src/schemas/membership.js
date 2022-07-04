6160518b42b3a5071c739033825fe2964903afb4
Sinethemba <sinethemba@wealthmigrate.com>
2022-07-05 01:23:51 +0200

    added new fields

--- src/schemas/membership.js
+++ src/schemas/membership.js
@@ -8,6 +8,8 @@ const membershipSchema = new Schema({
     id: { type: String, unique: true, sparse: true, default: v4() },
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    email: { type: String, required: true },
     id_number: { type: String, required: true },
     is_active: { type: Boolean, required: true, default: true },
     payment_type: { type: String, required: true },
