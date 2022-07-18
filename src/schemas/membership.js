42f109d410bd3e35f413bf3c441d8e097af200f9
Sinethemba <sinethemba@wealthmigrate.com>
2022-07-19 01:55:04 +0200

    fixed the type of the bank account number

--- src/schemas/membership.js
+++ src/schemas/membership.js
@@ -13,7 +13,7 @@ const membershipSchema = new Schema({
     id_number: { type: String, required: true },
     is_active: { type: Boolean, required: true, default: true },
     payment_type: { type: String, required: true },
-    bank_account_number: { type: Number },
+    bank_account_number: { type: String },
     contract_type: { type: String, required: true },
     amount: { type: Number, required: true}
 });
